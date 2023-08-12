7539b7d5bcb591a9ba4d2d1b68415ccc100ebb17
Felipe Torres <felipeost.torres@gmail.com>
2023-08-12 19:52:11 -0300

    feat: adding env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,3 @@
-// const API_URL = process.env.API_URL || 'http://localhost:3000';
-const API_URL = 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : 'http://localhost:3000'
 
 export { API_URL };
